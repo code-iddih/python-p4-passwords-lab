ec8dd2830f0860ab5e50f036afec9273016dbdbd
code-iddih <idinyamwaya@gmail.com>
2024-10-10 19:24:48 +0300

    Adds the Password Protection Functionality

--- server/app.py
+++ server/app.py
@@ -1,22 +1,15 @@
-#!/usr/bin/env python3
-
 from flask import request, session
 from flask_restful import Resource
-
 from config import app, db, api
 from models import User
 
 class ClearSession(Resource):
-
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
 
 class Signup(Resource):
-    
     def post(self):
         json = request.get_json()
         user = User(
@@ -28,16 +21,37 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            return user.to_dict(), 200
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter_by(username=json['username']).first()
+        
+        if user and user.authenticate(json['password']):
+            session['user_id'] = user.id
+            return {
+                'id': user.id,
+                'username': user.username
+            }, 200
+        
+        return {'message': 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
